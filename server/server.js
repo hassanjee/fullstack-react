d4c45082e909f12f801bf53524a08be05ce773d2
Hassan Javed <hjaved.engineer@gmail.com>
2026-02-10 19:31:06 +0300

    port binding

--- server/server.js
+++ server/server.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
    console.log(`Server is running on port http://localhost:${PORT}`)
 })
